e9269258c85cf3617a80a4ef34ffcf3b669c84ff
Abdul Ahad <adahadd4845e@gmail.com>
2025-03-19 02:39:28 +0600

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from './components/ThemeProvider';
-import './styles/globals.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" enableSystem>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
